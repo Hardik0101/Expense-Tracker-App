6c4d03c52706857ce434f631b7a9890df29489a6
Hardik0101 <hardik.prajapati@seaflux.tech>
2024-02-04 16:38:19 +0530

    Completed

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -5,7 +5,11 @@ import { ExpensesContext } from "../store/expenses-context";
 function AllExpenses() {
   const expenseCtx = useContext(ExpensesContext);
   return (
-    <ExpensesOutput expenses={expenseCtx.expenses} expensesPeriod="Total" />
+    <ExpensesOutput
+      expenses={expenseCtx.expenses}
+      expensesPeriod="Total"
+      fallbackText="No registerd expenses found !"
+    />
   );
 }
 
