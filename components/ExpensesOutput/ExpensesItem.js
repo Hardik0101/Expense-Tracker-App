2ab0c64fc11c6db1ec121cbe4329ef27437e94cd
Hardik0101 <hardik.prajapati@seaflux.tech>
2024-02-04 14:12:02 +0530

    Added  trash buttion icon

--- components/ExpensesOutput/ExpensesItem.js
+++ components/ExpensesOutput/ExpensesItem.js
@@ -4,10 +4,10 @@ import { GlobalStyles } from "../../constants/styles";
 import { getFormattedDate } from "../../util/date";
 import { useNavigation } from "@react-navigation/native";
 
-function ExpenseItem({ description, amount, date }) {
+function ExpenseItem({ description, amount, date, id }) {
   const navigation = useNavigation();
   function expensePressHandler() {
-    navigation.navigate("ManageExpense");
+    navigation.navigate("ManageExpense", { expenseId: id });
   }
   return (
     <Pressable
--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,7 +1,48 @@
-import { Text } from "react-native";
+import { useLayoutEffect } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import IconButton from "../UI/IconButton";
+import { GlobalStyles } from "../constants/styles";
 
-function ManageExpense() {
-  return <Text>ManageExpense Screen</Text>;
+function ManageExpense({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
+  function deleteExpenseHendler() {}
+  return (
+    <View style={styles.container}>
+      {isEditing && (
+        <View style={styles.deleteCOntainer}>
+          <IconButton
+            icon="trash"
+            size={36}
+            color={GlobalStyles.colors.error500}
+            onPress={deleteExpenseHendler}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteCOntainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
